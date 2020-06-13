a819174f918c1741c159741cdb75e3d6cb38fb99
Maximilian Burkhardt <squid219@gmail.com>
2020-06-13 16:44:32 -0700

    Reconfigure webpack to use new assets directory

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
                     to: path.resolve(__dirname, 'dist')
                 },
                 {
-                    from: path.resolve(__dirname, 'assets', '**', '*'),
+                    from: path.resolve(__dirname, 'guardian-assets', '**', '*'),
                     to: path.resolve(__dirname, 'dist')
                 }
             ]

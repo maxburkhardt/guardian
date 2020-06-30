e5d5aa3a3fb3e45af5df1951bcc2f5dea97d2019
Maximilian Burkhardt <squid219@gmail.com>
2020-06-30 12:20:06 -0700

    Remove duplicate files caused by OSX fs weirdness

--- src/config/Server.ts
+++
@@ -1,2 +0,0 @@
-export const SERVER_FPS = 30;
-export const SERVER_PORT = 9090;
--- src/util/CommunicationSignals.ts
+++
@@ -1,9 +0,0 @@
-export const SIGNALS = {
-  READY: "r",
-  INPUT: "i",
-  GAME_CREATE: "c",
-  GAME_JOIN: "j",
-  ENTER_LOBBY: "l",
-  GAME_START_REQUEST: "s",
-  GAME_START_NOTIFICATION: "n",
-};
